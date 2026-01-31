b2d67731814d569ee8de365c816023482e1df71c
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2026-01-31 18:28:44 +0100

    feat: test cons

--- paxostob/tob_test.go
+++ paxostob/tob_test.go
@@ -9,56 +9,63 @@ import (
 	"github.com/ynishimi/paxos-tob/paxostob/testutil"
 )
 
-// mockConsensus
-type mockConsensus struct {
+// mockCons
+type mockCons struct {
 	ch chan paxostob.Message
 }
 
-func newMockCons() *mockConsensus {
-	return &mockConsensus{
+func newMockCons() *mockCons {
+	return &mockCons{
 		ch: make(chan paxostob.Message),
 	}
 }
 
-func (c *mockConsensus) Propose(msg paxostob.Message) {
+func (c *mockCons) Propose(msg paxostob.Message) {
 	c.ch <- msg
 }
 
-func (c *mockConsensus) Decide() <-chan paxostob.Message {
+func (c *mockCons) Decide() <-chan paxostob.Message {
 	return c.ch
 }
 
-func (c *mockConsensus) Close() {
+func (c *mockCons) Close() {
 	close(c.ch)
 }
 
 func TestTobBroadcasterBroadcastSolo(t *testing.T) {
+	const NumPeers = 1
+	p1 := paxostob.NewInmemTransport("peer1")
 	tests := []struct {
 		name string // description of this test case
 		// Named input parameters for receiver constructor.
 		c paxostob.Consensus
 		// Named input parameters for target function.
-		msg paxostob.Message
+		msg       paxostob.Message
+		transport paxostob.Transport
 	}{
 		// TODO: Add test cases.
 		struct {
-			name string
-			c    paxostob.Consensus
-			msg  paxostob.Message
+			name      string
+			c         paxostob.Consensus
+			msg       paxostob.Message
+			transport paxostob.Transport
 		}{
-			name: "mockCons",
-			c:    newMockCons(),
-			msg:  testutil.NewTestMsg("peer1", "p1's message"),
+			name:      "mockCons",
+			c:         newMockCons(),
+			msg:       testutil.NewTestMsg("peer1", "p1's message"),
+			transport: p1,
+		},
+		{
+			name:      "cons",
+			c:         paxostob.NewCons(p1, 1, NumPeers),
+			msg:       testutil.NewTestMsg("peer1", "p1's message"),
+			transport: p1,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			const NumPeers = 1
-
-			p1 := paxostob.NewInmemTransport("peer1")
-
-			b := paxostob.NewTobBroadcaster(p1, 1, NumPeers, tt.c)
+			b := paxostob.NewTobBroadcaster(tt.transport, 1, NumPeers, tt.c)
 			b.Broadcast(tt.msg)
 
 			// should deliver the value
